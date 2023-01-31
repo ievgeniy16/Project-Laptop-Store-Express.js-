6f13549c4dc6e8117d3f1adf7731ff2956bd7f56
Evgeniy Kirnev <jokert3@mail.ru>
2023-01-31 10:41:35 +0200

    Created app server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const http = require('http');
-
-const server = http.createServer(() => {
-    console.log('Request received');
+const express = require('express');
+const hbs = require('express-handlebars').create({
+    extname: '.hbs'
 });
 
-server.listen(3000);
+const app = express();
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended: true}));
+app.use('/static', express.static('static'));
+
+app.listen(3000, ()=> console.log('Server listening on port 3000'));
